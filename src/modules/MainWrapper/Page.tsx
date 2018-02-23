a53990b7aa935d2e249b7f97741b015bf2fb3dda
wanghairong-i <3189578410@qq.com>
2018-02-23 16:28:12 +0800

    up tslint

--- src/modules/MainWrapper/Page.tsx
+++ src/modules/MainWrapper/Page.tsx
@@ -1,40 +1,36 @@
-import * as React from 'react'
-import { connect } from 'dva'
-import { DatePicker, Button, Card, List, Avatar, Icon } from 'antd'
-import classnames from 'classnames'
-import { Link, withRouter } from 'dva/router'
-import { LAYOUT_NAMESPACE } from 'configs/ConstConfig'
-import WithCommonProps from 'domainComponents/WithCommonProps'
-import Demo from './demo'
-const styles = require("./styles.less")
+import * as React from 'react';
+import { connect } from 'dva';
+import { DatePicker, Button, Card, List, Avatar, Icon } from 'antd';
+import classnames from 'classnames';
+import { Link, withRouter } from 'dva/router';
+import { LAYOUT_NAMESPACE } from 'configs/ConstConfig';
+import WithCommonProps from 'domainComponents/WithCommonProps';
+import Demo from './demo';
+const styles = require('./styles.less');
 
 function thefn(cb: (n: number) => number): number {
-  return cb(3)
+  return cb(3);
 }
 
-thefn(() => 3)
+thefn(() => 3);
 
 type ns = number | string;
 let n: ns = 'name';
 
 type cb = (n: string) => void;
-let fn:cb = (n: string): void => { /**/ }
-
+let fn: cb;
+fn = (n: string): void => { /**/ };
 
 type effects = 'scroll' | 'routate' | 'scale';
 function animate(effect: effects) { /**/ }
-animate('routate')
+animate('routate');
 
-
-class C3{
+class C3 {
   public name: string;
-  constructor(name){
-    this.name = name
+  constructor(name: string) {
+    this.name = name;
   }
 }
-
-
-
 
 // TS环境下：¨
 // let n = 0;
@@ -42,17 +38,16 @@ class C3{
 // let n: number = 0;
 // n = []; // Error
 
-
-
 // 尽可能地使用--strictNullChecks
 
+// tslint:disable-next-line:one-line
 interface Person{
-  name: string,
+  name: string;
   age: number;
-  readonly childs: Array<number>,
-  friends: string[],
-  shoes?: string, // 默认每项都是必传项，？指非必传项 
-  other: any, //any 可以指定任意数据类型，单不能不传
+  readonly childs: Array<number>;
+  friends: string[];
+  shoes?: string; // 默认每项都是必传项，？指非必传项 
+  other: any; // any 可以指定任意数据类型，单不能不传
 }
 
 function greeter(person: Person): string {
@@ -62,30 +57,27 @@ function greeter(person: Person): string {
   // person.childs = []; //Error
   person.childs.push(3);
   
-  other = 123 //OK
-  let strLength: number = (name as string).length; //类型断言， 当使用JSX时，只有 as语法断言是被允许的
-  return `${name}: ${age} - ${childs.join()} - ${friends.join()} - ${other}`
+  other = 123; //OK
+  let strLength: number = (name as string).length; // 类型断言， 当使用JSX时，只有 as语法断言是被允许的
+  return `${name}: ${age} - ${childs.join()} - ${friends.join()} - ${other}`;
 }
 
 function hello(params: { name: string }): any {
-  return name
+  return name;
 }
-hello({ name: '12' })
-
-
+hello({ name: '12' });
 
 // 一个函数没有返回值时， 用void
-function returnUndefined(params:any): void {
+function returnUndefined(params: any): void {
   console.log(params);
   // return 't'; // Error
 }
-
 
 // 泛型  ： 使返回值的类型与传入参数的类型是相同的
 // function identity(arg: any): any { //无法确定传入参数和返回数据类型一致
 //   return arg;
 // }
-function identity<T>(arg: T): T { //T： 类型变量
+function identity<T>(arg: T): T { // T： 类型变量
   return arg;
 }
 
@@ -97,7 +89,6 @@ let arr: Array<number> = [1, 2];
 
 let x: (number | string) = 3;
 
-
 class C {
   public x: number;
   a: string;
@@ -108,52 +99,47 @@ class C {
   }
 }
 
-let c = new C(1, '2')
+let c = new C(1, '2');
 console.log(c);
 // console.log(c.c); //Error 属性c是私有属性，只能在类C中访问
-
 
 let d: ReadonlyArray<number> = [1, 3, 5];
 // d.push(7); //Error ReadonlyArray把数组所有可变方法去掉了，确保数组创建后再也不能被修改
 
 interface N{
-  n1: number,
-  n2: string
+  n1: number;
+  n2: string;
 }
-function fn1(params:N) { /**/ }
-fn1({ n1: 3 } as N)
-
-
+function fn1(params: N) { /**/ }
+fn1({ n1: 3 } as N);
 
 class C2 {
   private c: boolean;
   readonly d: boolean;
-  constructor(c = false, d = true) {
-    this.c = c; //外界无法通过实例访问c属性
+  constructor(c: boolean = false, d: boolean = true) {
+    this.c = c; // 外界无法通过实例访问c属性
     this.d = d; // 只读属性必须在声明时或构造函数里被初始化
   }
 }
 
 const mapStateToProps = state => {
   return {
-  }
-}
+  };
+};
 interface G2{
-  name?: string
+  name?: string;
 }
 @withRouter
 @WithCommonProps
 @connect(mapStateToProps)
 class IndexPage extends React.Component<any, any>{
-  constructor(props) {
-    super(props)
-  }
   render() {
     const { dispatch, children, theme } = this.props;
     const pageClasses = classnames({
-      [styles["page"]]: true,
+      // tslint:disable-next-line:no-string-literal
+      [styles['page']]: true,
       [styles[theme]]: true
-    })
+    });
     
     return (
       <div className={pageClasses}>
@@ -161,14 +147,13 @@ class IndexPage extends React.Component<any, any>{
         <Demo />
         <DatePicker />
         <Icon type="link" />
-        <Button type="primary">{greeter({ name: 'whr', age: 3, childs: [1, 2], friends: ['she', 'he'], other: '哈哈' })}</Button>
+        <Button type="primary">
+          {greeter({ name: 'whr', age: 3, childs: [1, 2], friends: ['she', 'he'], other: '哈哈' })}
+        </Button>
       </div>
     );
   }
 }
-
-
-
 
 // 1，限制参数有且必须有哪些属性及类型？
 // interface N {
@@ -193,5 +178,4 @@ class IndexPage extends React.Component<any, any>{
 
 // 关于类，几个好用的技能点：
 
-
-export default IndexPage
+export default IndexPage;
