d40f1de2413699f61d83448e169c23a66c43b558
wanghairong-i <3189578410@qq.com>
2018-02-23 20:13:47 +0800

    up demo

--- src/modules/MainWrapper/demo.tsx
+++ src/modules/MainWrapper/demo.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { Button } from 'antd';
 import request from '../../utils/request';
 
-interface G {
-  readonly n: number;
-  m: string[];
+class Y {
+	game: 'string'; // 默认所有的属性和方法都是 public 的
+	private name: string = 'whr-name';
+	protected age: number = 18;
+	public find(): string {
+		return this.name;
+	}
 }
 
-let a: G;
-
-class Comp extends React.Component {
-  /**
-   * method
-   * @param 
-   */
-  public method() {
-    request('');
-  }
-  render() {
-    return (
-      <Button>点我</Button>
-    );
-  }
+class X extends Y {
+	public log(): void {
+		console.log(this.name); // Error
+		console.log(this.age);
+		console.log(this.game);
+	}
 }
 
-export default Comp;
+let y1 = new Y();
+// 以上是下面的简写：
+// let y1: Y = new Y();
+
+console.log(y1.name); // Error
+console.log(y1.age); // Error
+console.log(y1.game);
+console.log(y1.find());
